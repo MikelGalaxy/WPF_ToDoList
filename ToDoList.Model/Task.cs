1dde6d876109164210ba060334f89ceae732900b
Michał Boniakowski <michal.boniakowski11@gmail.com>
2018-05-14 18:18:53 +0200

    Initial

---
+++ ToDoList.Model/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Model
+{
+    public class Task
+    {
+        public bool Checked { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Title { get; set; }
+        public int Completion { get; set; }
+        public string Description { get; set; }
+    }
+}
